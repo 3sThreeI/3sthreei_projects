b6a9a772a60c8308cba10b6b4e1076c988fcc08d
AbouCloud <Abouzari6839@gmail.com>
2026-03-02 10:51:49 +0000

    hero completed and services section started

--- components/ui/customButton/switchLang.tsx
+++ components/ui/customButton/switchLang.tsx
@@ -3,28 +3,32 @@
 import { useRouter } from "next/navigation";
 import "./switchLang.css"
 import { useEffect, useState } from "react";
-function SwitchButtonTheme({ locale }: any) {
+
+interface HeroProps {
+    locale: string
+}
+function SwitchButtonTheme({ locale }: HeroProps) {
     const [active, setActive] = useState(locale)
     const router = useRouter()
     const Switchlanguage = () => {
-      const newLocale = active === "fr" ? "en" : "fr"
+        const newLocale = active === "fr" ? "en" : "fr"
 
-    setActive(newLocale)
-            setTimeout(() => {
-                if (locale === 'fr') {
-                    router.push('/en')
-                } else {
-                    router.push('/fr')
-                }
-            }, 300);
+        setActive(newLocale)
+        setTimeout(() => {
+            if (locale === 'fr') {
+                router.push('/en')
+            } else {
+                router.push('/fr')
+            }
+        }, 300);
 
     }
     return (
         <>
-            <div className="border border-(--theme-btn-color) h-6 w-12 bg-(--theme-btn-color) rounded-full text-center cursor-pointer" onClick={Switchlanguage}>
+            <div className="themeBtn" onClick={Switchlanguage}>
                 <div className={` btn ${active == 'fr' ? 'active ' : "disactive"}`}
                 >
-                    <span className=" text-(--theme-btn-color) text-sm absolute left-0.5 font-bold">{locale === 'fr' ? 'En' : 'Fr'}</span>
+                    <span className="themeText">{locale === 'fr' ? 'En' : 'Fr'}</span>
                 </div>
             </div>
         </>
