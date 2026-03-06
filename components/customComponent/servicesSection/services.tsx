59731dac2cb40c3b7a7b7d469e2922de3551ad3a
AbouCloud <Abouzari6839@gmail.com>
2026-03-06 14:38:05 +0000

    why choose us page created but not design

--- components/customComponent/servicesSection/services.tsx
+++ components/customComponent/servicesSection/services.tsx
@@ -1,5 +1,6 @@
 import { getTranslations } from "next-intl/server"
 import style from "./serviceStyle.module.css"
+import Link from "next/link"
 
 export default async function OurService() {
     const t = await getTranslations('serviceSection')
@@ -9,52 +10,64 @@ export default async function OurService() {
                 <h1 className={style.title}>Ours Services</h1>
                 <div className={style.cards}>
                     {/* website */}
-                    <div className={`${style.card} ${style.card1}`}>
+                    <div className={`${style.card} `}>
                         <h1 className={style.card_title}>{t('website.title')}</h1>
                         <h2 className={style.card_subtitle}>{t('website.subtitle')}</h2>
                         <ul className={style.content}>
-                            {t.raw('website.contents').map((item:string, index:number)=>(
+                            {t.raw('website.contents').map((item: string, index: number) => (
                                 <li key={index} className={style.content_text}>
                                     {item}
                                 </li>
                             ))}
                         </ul>
+                        <div className={style.btn}>
+                            <Link href='#' className={style.Link} >{t('website.button')}</Link>
+                        </div>
                     </div>
                     {/* mobile app */}
-                    <div className={`${style.card} ${style.card2}`}>
+                    <div className={`${style.card}`}>
                         <h1 className={style.card_title}>{t('mobile.title')}</h1>
                         <h2 className={style.card_subtitle}>{t('mobile.subtitle')} </h2>
                         <ul className={style.content}>
-                            {t.raw('mobile.contents').map((item:string, index:number)=>(
+                            {t.raw('mobile.contents').map((item: string, index: number) => (
                                 <li key={index} className={style.content_text}>
                                     {item}
                                 </li>
                             ))}
                         </ul>
+                        <div className={style.btn}>
+                            <Link href='#' className={style.Link} >{t('mobile.button')}</Link>
+                        </div>
                     </div>
                     {/* gaming */}
-                    <div className={`${style.card} ${style.card3}`}>
+                    <div className={`${style.card}`}>
                         <h1 className={style.card_title}>{t('game.title')}</h1>
                         <h2 className={style.card_subtitle}>{t('game.subtitle')}</h2>
                         <ul className={style.content}>
-                            {t.raw('game.contents').map((item:string, index:number) => (
+                            {t.raw('game.contents').map((item: string, index: number) => (
                                 <li key={index} className={style.content_text}>
                                     {item}
                                 </li>
                             ))}
                         </ul>
+                        <div className={style.btn}>
+                            <Link href='#' className={style.Link} >{t('game.button')}</Link>
+                        </div>
                     </div>
                     {/* ui/ux */}
-                    <div className={`${style.card} ${style.card4}`}>
+                    <div className={`${style.card}`}>
                         <h1 className={style.card_title}>{t('design.title')}</h1>
                         <h2 className={style.card_subtitle}>{t('design.subtitle')}</h2>
                         <ul className={style.content}>
-                            {t.raw('design.contents').map((item:string, index:number)=>(
+                            {t.raw('design.contents').map((item: string, index: number) => (
                                 <li key={index} className={style.content_text}>
                                     {item}
                                 </li>
                             ))}
                         </ul>
+                        <div className={style.btn}>
+                            <Link href='#' className={style.Link} >{t('design.button')}</Link>
+                        </div>
                     </div>
                 </div>
             </div>
