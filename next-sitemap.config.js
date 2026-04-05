66c0f33ead48be8c394ded81201b7c46d72579a2
AbouCloud <Abouzari6839@gmail.com>
2026-04-05 13:12:35 +0000

    site map fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,16 @@
-/** @type {import('next-sitemap').IConfig} */
+
 module.exports = {
-  siteUrl: 'https://3sthreei.com', // your domain
-  generateRobotsTxt: true,        // optional, generates robots.txt
-  sitemapSize: 5000,               // optional, limits pages per sitemap
-  changefreq: 'daily',             // optional, how often pages change
-  priority: 0.7,                   // optional, default priority for pages
+  siteUrl: 'https://3sthreei.com',
+  generateRobotsTxt: true,
+
+  // Force include homepage
+  additionalPaths: async () => {
+    return [
+      {
+        loc: '/',
+        changefreq: 'daily',
+        priority: 1.0,
+      },
+    ]
+  },
 }
