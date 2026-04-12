9bd651aab52fb048dbbdcb945b47c34d6c7b32f4
AbouCloud <Abouzari6839@gmail.com>
2026-04-12 12:57:37 +0000

    testimonial page updated and completed

--- app/[locale]/assets/testimonial/testimonial.tsx
+++ app/[locale]/assets/testimonial/testimonial.tsx
@@ -2,41 +2,51 @@ import { useTranslations } from "next-intl"
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
 import style from "./testimonial.module.css"
 type TestimonialProps = {
-    imgUrl: string,
-    contentText: string
+    imgUrl: string | null,
+    name: string,
+    text: string
 }
 export default function Testimonial() {
     const t = useTranslations()
     const items = t.raw('testimonial') as TestimonialProps[]
     return (
         <>
-            <div className={style.conatainer}>
-                <h1 className={style.logo}>3SThreeI</h1>
-                <h2 className={style.title}>What Our Clients Say About Us</h2>
-                <Carousel>
+            <div className={style.container}>
+                <div className={style.logoContainer}>
+                    <h1 className={style.logo}>3SThreeI</h1>
+                    <h2 className={style.subtitle}>What Our Clients Say About Us</h2>
+                </div>
+                <Carousel className={style.carousel} opts={{
+                        align: "start",
+                        loop: true,
+                    }}>
                     <CarouselContent className={style.cards}>
                         {
                             items.map((item, index) => (
-                                <CarouselItem key={index}>
-                                    {
-                                        item.imgUrl && item.imgUrl.length > 0
-                                            ?
-                                            (
-                                                <div className={style.img}>
-                                                    <img src="#" alt="" />
-                                                </div>
-                                            ) : 
-                                            // if the image is not available we will display empty card with design
-                                            (
-                                                <div className={style.img}>
-                                                    <div className={style.cardImage}></div>
-                                                </div>
-                                            )
-                                    }
-
-                                    <div className={style.contentText}>
-                                        {item.contentText}
+                                <CarouselItem className={style.carouselItem} key={index}>
+                                    <div className={style.card}>
+                                        <div className={style.contentText}>
+                                            {item.text}
+                                        </div>
+                                        {
+                                            item.imgUrl && item.imgUrl.length > 0
+                                                ?
+                                                (
+                                                    <div className={style.imgContainer}>
+                                                        <img src="#" alt="" className={style.img}/>
+                                                         <p className={style.UserName}>{item.name}</p>
+                                                    </div>
+                                                ) :
+                                                // if the image is not available we will display empty card with design
+                                                (
+                                                    <div className={style.imgContainer}>
+                                                        <div className={style.cardImage}></div>
+                                                        <p className={style.UserName}>{item.name}</p>
+                                                    </div>
+                                                )
+                                        }
                                     </div>
+
                                 </CarouselItem>
                             ))
                         }
