f49052ae2c3c01278c4f8a41d8bad70991d20d6e
AbouCloud <Abouzari6839@gmail.com>
2026-05-11 09:51:54 +0000

    navbar editing

--- components/customComponent/navbar/Navbar.tsx
+++ components/customComponent/navbar/Navbar.tsx
@@ -1,41 +1,58 @@
-// "use client"
+
 import { getTranslations } from "next-intl/server";
 import Link from "next/link";
 import { getLocale } from "next-intl/server";
-import SwitchButtonTheme from "../../ui/customButton/switchLang";
-import { TiArrowSortedDown } from "react-icons/ti";
+import SwitchButtonLang from "../../ui/customButton/switchLang";
 import PhoneNavbar from "./PhonNavbar";
 import style from "./navbar.module.css"
+import LargeScreenNavbar from "./largeScreenNavbar";
+
+export interface NavbarProps {
+    home: string;
+    keyService: string;
+    servicesValue: [
+        web:string,
+        modbile: string,
+        gaming:string,
+        design:string
+    ] ;
+    project: string;
+    about: string;
+    faq: string;
+    blog: string;
+    signin: string;
+}
 export default async function Navbar() {
     const t = await getTranslations('navbar')
     const locale = await getLocale()
+    // creating the navbar data for sharing with different components
+    const navData: NavbarProps = {
+    home: t('home'),
+    keyService: t('keyservice'),
+    servicesValue: t.raw('servicesValue') as any,
+    project: t('project'),
+    about: t('about'),
+    faq: t('faq'),
+    blog: t('blog'),
+    signin: t('signin')
+  };
+
     return (
         <nav className={style.nav}>
             <div className="py-2">
                 <h1>3SThreeI</h1>
             </div>
             {/* -------------------------- huge screee */}
-            <ul className={style.navlink}>
-                <li> <Link href='/'>{t('home')}</Link></li>
-                <li className={style.li}>
-                    {/* <Link href='/service' > */}
-                        {t('service')} <TiArrowSortedDown className={style.icon} />
-                    {/* </Link> */}
-                </li>
-                <li> <Link href='/projects'>{t('project')}</Link></li>
-                <li> <Link href='/about'>{t('about')}</Link></li>
-                <li> <Link href='/faq'>{t('faq')}</Link></li>
-                <li> <Link href='/blog'>{t('blog')}</Link></li>
-                <li> <Link href='/blog'>{t('signin')}</Link></li>
-            </ul>
+            <div className="">
+                <LargeScreenNavbar {...navData} />
+            </div>
             {/* -------------------------- small screen */}
             <div className={style.button}>
-                {/* button for theme */}
-                <SwitchButtonTheme locale={locale} />
                 {/* button to switch language  */}
+                <SwitchButtonLang locale={locale} />
+                <PhoneNavbar {...navData} />
                 {/* <div className="border h-5 w-14">
                 </div> */}
-                <PhoneNavbar home={t('home')} service={t('service')} project={t('project')} about={t('about')} faq={t('faq')} blog={t('blog')} signin={t('signin')} />
             </div>
         </nav>
     );
