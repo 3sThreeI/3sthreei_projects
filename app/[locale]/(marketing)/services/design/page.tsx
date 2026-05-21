e6cf7b01cceaa2385da1ef84dbca3b763a3595bc
AbouCloud <Abouzari6839@gmail.com>
2026-05-21 22:28:37 +0000

    service deisgn  page updated

--- app/[locale]/(marketing)/services/design/page.tsx
+++ app/[locale]/(marketing)/services/design/page.tsx
@@ -1,12 +1,37 @@
 import ServicesHero from "@/components/customComponent/servicesHero/ServicesHero";
+import { ParamsPropsServices } from "../web/page";
+import { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
 
-
-export default function Design(){
+export async function generateMetadata(params: Promise<{ locale: string }>): Promise<Metadata> {
+    const { locale } = await params
+    const t = await getTranslations({ locale: locale, namespace: "services" })
+    return {
+        title: t("design.title"),
+        description: t("design.description"),
+        keywords: t.raw("design.keywords").join(", "),
+        openGraph: {
+            title: t("design.title"),
+            description: t("design.description"),
+            url: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}/services/design`
+        },
+        alternates: {
+            canonical: `/${locale}/services/design`
+        },
+        robots: {
+            index: true,
+            follow: true,
+        }
+    }
+}
+export default async function Design({ params }: ParamsPropsServices) {
+    const { locale } = await params;
+    const messages = (await import(`@/messages/${locale}/services/design.json`))
     return (
         <>
-        <section>
-            <ServicesHero title="Custom web and application design" Planguage="Figma • Adobe XD"/>
-        </section>
+            <section>
+                <ServicesHero messages={messages} />
+            </section>
         </>
     )
 }
