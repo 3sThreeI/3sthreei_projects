4ce7b389727ae964a960f4258cc48492485b7134
AbouCloud <Abouzari6839@gmail.com>
2026-05-21 22:29:38 +0000

    working price edited

--- app/[locale]/assets/workFlow/workingPricing.tsx
+++ app/[locale]/assets/workFlow/workingPricing.tsx
@@ -3,22 +3,34 @@ import style from "./workingPricing.module.css"
 import WorkingFlowCard from "./workingPricingCard"
 type workingProps = {
     header: string,
-    content: string
+    content: []
 }
-export default async function WorkingFlow() {
-    const t = await getTranslations()
-    const items = t.raw('workFlow') as workingProps[]
-    const data = t.raw("workFlow");
+export default async function WorkingFlow(messages:any) {
+    // in home page the message this component is rendered there too but is not sanding the data through props
+    // so w will get it in translations in home.json 
+    const messageItems = await messages.messages
+    const t = messageItems ?? await getTranslations()
+    console.log("workingPricing rendered")
+    // if the messages exist then the contain is an function otherwise is an object 
+    // the workflow is the table and worklow2 is the card 
+    const data:workingProps[] = !messages ? t.raw("workFlow") : t.workFlow
+    console.log("data")
     return (
         <>
             <div className={style.container}>
                 < WorkingFlowCard />
+                {/* this the table part for the features */}
                 <div className={`${style.tableContent}`}>
                     <table className={style.table}>
                         <thead className={style.thead}>
                             <tr className={style.head_row}>
                                 {
+                                    !messageItems ?
                                     t.raw('workFlow').map((item: any, index: number) => (
+                                        <th className={`${style.header} `} key={index}>{item.header}</th>
+                                    ))
+                                    : 
+                                    data.map((item: any, index: number) => (
                                         <th className={`${style.header} `} key={index}>{item.header}</th>
                                     ))
                                 }
