a48b247951ef900a459849a3fa5da3ee544f1253
AbouCloud <Abouzari6839@gmail.com>
2026-05-21 22:28:14 +0000

    service application  page updated

--- app/[locale]/(marketing)/services/application/page.tsx
+++ app/[locale]/(marketing)/services/application/page.tsx
@@ -1,12 +1,40 @@
 import ServicesHero from "@/components/customComponent/servicesHero/ServicesHero";
+import { ParamsPropsServices } from "../web/page";
+import { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
 
+export async function generateMetadata(params: Promise<{ locale: string }>): Promise<Metadata> {
+    const { locale } = await params
+    const t = await getTranslations({ locale: locale, namespace: "services" })
+    const sitename = await getTranslations('sitenames') as any
+    return {
+        title: t("application.title"),
+        description: t("application.description"),
+        keywords: t.raw("application.keywords").join(", "),
+        openGraph: {
+            title: t("application.title"),
+            description: t("application.description"),
+            siteName: sitename,
+            url: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}/services/application`
+        },
+        "alternates": {
+            canonical: `/${locale}/services/application`
+        },
+        robots: {
+            index: true,
+            follow: true,
+        }
+    }
+}
 
-export default function Application(){
+export default async function Application({ params }: ParamsPropsServices) {
+    const { locale } = await params
+    const messages = (await import(`@/messages/${locale}/services/application.json`))
     return (
         <>
-        <section>
-            <ServicesHero title="Custom mobile and deskop application" Planguage="Flutter • React Native • ElectronJS • JAVA"/>
-        </section>
+            <section>
+                <ServicesHero messages={messages} />
+            </section>
         </>
     )
 }
