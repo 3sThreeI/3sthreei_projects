ca4c37952d6a46ab3667e8b43b88cb39b3f523b4
AbouCloud <Abouzari6839@gmail.com>
2026-05-28 18:50:30 +0000

    workingPrincing updated

--- app/[locale]/assets/workFlow/workingPricing.tsx
+++ app/[locale]/assets/workFlow/workingPricing.tsx
@@ -5,7 +5,7 @@ type workingProps = {
     header: string,
     content: []
 }
-export default async function WorkingFlow(messages:any) {
+export default async function WorkingFlow(messages: any) {
     // in home page the message this component is rendered there too but is not sanding the data through props
     // so w will get it in translations in home.json 
     const messageItems = await messages.messages
@@ -13,8 +13,8 @@ export default async function WorkingFlow(messages:any) {
     console.log("workingPricing rendered")
     // if the messages exist then the contain is an function otherwise is an object 
     // the workflow is the table and worklow2 is the card 
-    const data:workingProps[] = !messages ? t.raw("workFlow") : t.workFlow
-    console.log("data")
+    const data: workingProps[] = !messageItems ? t.raw("workFlow") : t.workFlow
+    console.log("data", typeof (messageItems))
     return (
         <>
             <div className={style.container}>
@@ -26,24 +26,24 @@ export default async function WorkingFlow(messages:any) {
                             <tr className={style.head_row}>
                                 {
                                     !messageItems ?
-                                    t.raw('workFlow').map((item: any, index: number) => (
-                                        <th className={`${style.header} `} key={index}>{item.header}</th>
-                                    ))
-                                    : 
-                                    data.map((item: any, index: number) => (
-                                        <th className={`${style.header} `} key={index}>{item.header}</th>
-                                    ))
+                                        t?.raw('workFlow').map((item: any, index: number) => (
+                                            <th className={`${style.header} `} key={index}>{item.header}</th>
+                                        ))
+                                        :
+                                        data?.map((item: any, index: number) => (
+                                            <th className={`${style.header} `} key={index}>{item.header}</th>
+                                        ))
                                 }
                             </tr>
                         </thead>
                         <tbody className={style.table_body}>
-                                {data[0].content.map((_: any, rowIndex: number) => (
-                                    <tr className={style.body_row} key={rowIndex}>
-                                        {data.map((item: any, colIndex: number) => (
-                                            <td className={`${style.body_col}`} key={colIndex}>{item.content[rowIndex]}</td>
-                                        ))}
-                                    </tr>
-                                ))}
+                            {data?.[0]?.content?.map((_: any, rowIndex: number) => (
+                                <tr className={style.body_row} key={rowIndex}>
+                                    {data?.map((item: any, colIndex: number) => (
+                                        <td className={`${style.body_col}`} key={colIndex}>{item.content[rowIndex]}</td>
+                                    ))}
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
                 </div>
