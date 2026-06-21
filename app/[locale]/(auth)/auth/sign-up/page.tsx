d0c5dc11da42f29b598802acfee64952983281c8
AbouCloud <Abouzari6839@gmail.com>
2026-06-21 23:41:32 +0000

    auth api added

---
+++ app/[locale]/(auth)/auth/sign-up/page.tsx
@@ -0,0 +1,47 @@
+import { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
+import style from "../sign-in/sign.module.css"
+import { ParamProps } from "@/app/[locale]/(marketing)/about/page";
+import SignUpComp from "@/app/[locale]/assets/auth/signUp";
+export async function generateMetadata(params: Promise<{ locale: string }>): Promise<Metadata> {
+    const { locale } = await params
+    const t = await getTranslations({ locale, namespace: "signIn" })
+    const sitename = await getTranslations('sitenames') as any
+    return {
+        title: t('title'),
+        description: t('description'),
+        keywords: t.raw('keywords').join(', '),
+        openGraph: {
+            title: t('title'),
+            description: t('description'),
+            siteName: sitename,
+            url: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}/projects`
+        },
+        alternates: {
+            canonical: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}/projects`,
+            languages: {
+                fr: `${process.env.NEXT_PUBLIC_SITE_URL}/fr`,
+                en: `${process.env.NEXT_PUBLIC_SITE_URL}/en`
+            }
+        },
+        robots: {
+            index: true,
+            follow: true
+        }
+    }
+}
+export default async function SignIn({ params }: ParamProps) {
+    const { locale } = await params
+    const t = (await import(`@/messages/${locale}/auth.json`))
+    return (
+        <main>
+            <div className={style.container}>
+                <div className={style.wrapper}>
+                    <SignUpComp messages={t.forms} />
+                    <div className={style.aside}>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
