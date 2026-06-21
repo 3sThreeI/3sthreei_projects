a36ef7d2812b986102db75393f76a7962f5e7376
AbouCloud <Abouzari6839@gmail.com>
2026-06-21 23:40:37 +0000

    authentification added

---
+++ app/api/auth/[signInUp]/route.ts
@@ -0,0 +1,21 @@
+import { callBackend } from "@/lib/api/backend";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function POST(request: NextRequest) {
+    try {
+        const pathname = new URL(request.url).pathname
+        console.log("abzar pathname", pathname)
+        const rawData = await request.json()
+        const data = await callBackend(pathname, rawData, "POST")
+        return NextResponse.json({ success: true, data: data })
+    } catch (error:any) {
+        return NextResponse.json(
+            {
+                success: false,
+                errors: error.message || "failed to submit" 
+            },
+            { status: 500 }
+        )
+    }
+}
