525487b5f5b681775bd1642a30249b5579c8259b
AbouCloud <Abouzari6839@gmail.com>
2026-06-25 21:46:09 +0000

    updating

--- lib/validation/contact.ts
+++ lib/validation/contact.ts
@@ -5,14 +5,16 @@ export const contactSchema = z.object({
     .min(5, 'Name must be at least 5 characters')
     .regex(/^[A-Z][a-z]+\s+[A-Z][a-z]+$/, "please type a validate Name, no space at the end" ),
     email: z.string().email("Invalid Email"),
-    phoneNumber: z.string()
+    phone_number: z.string()
         .min(7, "Phone number is too short")
         .trim()
         .max(20, "Phone number is too long")
         .regex(/^\+\d{1,4}\d{7,15}$/, "Please enter a valid phone number. (e.g., +233000000)"),  // ✅ More 
-    message: z
+    description: z
         .string()
-        .min(30, "Message must be at least 30 characters")
-        .max(250, "Message must be less than 250 characters")
+        .min(80, "Message must be at least 80 characters")
+        .max(250, "Message must be less than 250 characters"),
+    price: z.string().optional(),
+    project_type: z.string()
 })
 export type ContactForm = z.infer<typeof contactSchema>
--- lib/validation/testimonial.ts
+++ lib/validation/testimonial.ts
@@ -6,8 +6,8 @@ export const testimonialSchema = z.object({
         .regex(/^[A-Z][a-z]+\s+[A-Z][a-z]+$/, "please type a validate Name, no space at the end"),
     feedback: z
         .string()
-        .min(15, "Message must be at least 15 characters")
-        .max(80, "Message must be less than 80 characters"),
-    // file: z.file("please upload an image")
+        .min(60, "Message must be at least 60 characters")
+        .max(150, "Message must be less than 150 characters"),
+    Img_url: z.instanceof(File)
 })
 export type TestimonialSchema = z.infer<typeof testimonialSchema>
