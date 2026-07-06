4b6d05f190dccf16e418a841f6999d112fe94d77
AbouCloud <Abouzari6839@gmail.com>
2026-07-06 02:46:54 +0000

    ready to be push backend.ts updated

--- lib/api/backend.ts
+++ lib/api/backend.ts
@@ -1,23 +1,23 @@
-export async function callBackend<T>(
+export async function callBackend(
     path: string,
     body: any,
+    credentials: boolean = false,
     method: 'POST' | 'GET' | 'PUT' | 'DELETE' = 'POST'
-): Promise<T> {
+) {
     const isFormData = body instanceof FormData
-    const BaseUrl = process.env.NEXT_PUBLIC_BACKEND_URL ||  "http://localhost:8440"
+    const BaseUrl = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8440"
     // console.log("BAck-end: ", BaseUrl,path) 
     const resp = await fetch(`${BaseUrl}${path}`, {
         method,
         headers: {
             ...(isFormData ? {} : { 'Content-Type': 'application/json' }),
-            'Autorization': `Bearer ${process.env.BACKEND_API_KEY}`,
+            'Authorization': `Bearer ${process.env.BACKEND_API_KEY}`,
         },
-        body: method !== "GET" && method !== "DELETE" ? (isFormData ? body : JSON.stringify(body)) : undefined
+        body: method !== "GET" && method !== "DELETE" ? (isFormData ? body : JSON.stringify(body)) : undefined,
+        credentials: "include"
     })
-    console.log("My front-end backent.ts, Body: ", body || JSON.stringify(body))
-    const data = await resp.json()
-    if (!resp.ok) {
-        throw new Error(data.message || "Backend API failed")
-    }
-    return data
+    // console.log("My front-end backent.ts, Body: ", body || JSON.stringify(body))
+    // console.log(resp.headers.getSetCookie());
+    // console.log(resp.headers.get("set-cookie"));
+    return resp
 }
