dd10424e4dce4e4f8c62bf834400f2fffda59904
AbouCloud <Abouzari6839@gmail.com>
2026-07-06 23:47:16 +0000

    portfolio page updated

--- app/[locale]/assets/portfolio/portfolio.tsx
+++ app/[locale]/assets/portfolio/portfolio.tsx
@@ -9,8 +9,32 @@ type PortfolioSchema = {
     bgText: string,
     projectUrl:string
 }
-export default async function Portfolio() {
+async function fetchProject(type?:string|null){
+    const params = new URLSearchParams({
+        limit: "15",
+        page: "1",
+        type: type || ""
+    })
+    try {
+        const resp = await fetch(`http://localhost:8440/api/projects/getAll?${params}`, {
+            headers: {"Content-Type": "application/json"},
+            method: "GET"
+        })
+        const data = await resp.json()
+        if(!resp.ok){
+            console.log("Project Failed", data.Error)
+            return []
+        }
+        console.log("Project Fetch successfully", data)
+    } catch (error:any) {
+        console.log("Project failed ", error.message)
+        return []
+    }
+}
+export default async function Portfolio(type:any) {
     const t = await getTranslations()
+    const Project = await fetchProject()
+    console.log("Project ", Project)
     const items = t.raw('portfolio') as PortfolioSchema[]
     return (
         <>
