fe075d252e6494f6f826c5dd50e611de5d7954eb
AbouCloud <Abouzari6839@gmail.com>
2026-07-06 23:48:23 +0000

    navbar page updated

--- components/customComponent/navbar/Navbar.tsx
+++ components/customComponent/navbar/Navbar.tsx
@@ -6,6 +6,7 @@ import SwitchButtonLang from "../../ui/customButton/switchLang";
 import PhoneNavbar from "./PhonNavbar";
 import style from "./navbar.module.css"
 import LargeScreenNavbar from "./largeScreenNavbar";
+import { cookies } from "next/headers";
 
 export interface NavbarProps {
     home: string;
@@ -21,10 +22,42 @@ export interface NavbarProps {
     faq: string;
     blog: string;
     signin: string;
+    user: {
+        firstname:string,
+        lastname:string,
+        email:string,
+        role:string,
+        admin:boolean
+    }
+}
+async function fetchUser() {
+  const cookieStore = await cookies()
+  try {
+    const resp = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/refresh-me`, {
+      headers: {
+        Cookie: cookieStore.toString()
+      },
+      method: "POST",
+      credentials: "include",
+      cache: "no-store"
+    })
+    if (!resp.ok) {
+      console.log("refresh token failed")
+      return []
+    }
+    const data = await resp.json()
+    // console.log("User", data)
+    return data
+  } catch (error:any) {
+      console.log("refresh token failed", error.message)
+    return [];
+  }
 }
 export default async function Navbar() {
     const t = await getTranslations('navbar')
     const locale = await getLocale()
+    const data = await fetchUser()
+    const user = data?.user
     // creating the navbar data for sharing with different components
     const navData: NavbarProps = {
     home: t('home'),
@@ -34,7 +67,8 @@ export default async function Navbar() {
     about: t('about'),
     faq: t('faq'),
     blog: t('blog'),
-    signin: t('signin')
+    signin: t('signin'),
+    user: user
   };
 
     return (
