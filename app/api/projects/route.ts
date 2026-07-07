c15f213c4a2689e59af7492df3b4776328e956f5
AbouCloud <Abouzari6839@gmail.com>
2026-07-07 15:18:53 +0000

    api route updated for projects

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -2,7 +2,7 @@ import { callBackend } from "@/lib/api/backend";
 import { projectsSchema } from "@/lib/validation/project";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: NextRequest, response: NextResponse) {
+export async function POST(request: NextRequest) {
     try {
         const formdata = await request.formData()
         const rawData = {
@@ -12,17 +12,18 @@ export async function POST(request: NextRequest, response: NextResponse) {
             img_url: formdata.get("img_url") as File | null,
             type: formdata.get("type"),
         }
-        console.log("raw data", rawData)
+        // console.log("raw data", rawData)
         const resp = await callBackend(`/api/projects/create`, formdata, false, "POST")
         const data = await resp.json()
         if (!resp.ok) {
+            console.log("not should be here")
             return NextResponse.json(
                 { errors: data.message || "failed to submit" },
                 { status: 500 }
             )
         }
         return NextResponse.json({
-            data: await resp.json(),
+            data: data,
             success: true
         }, { status: 200 })
     } catch (error: any) {
@@ -32,7 +33,7 @@ export async function POST(request: NextRequest, response: NextResponse) {
         )
     }
 }
-export async function GET(request: NextRequest, response: NextResponse) {
+export async function GET(request: NextRequest) {
     try {
         const searchParams = request.nextUrl.searchParams
         const query = Object.fromEntries(searchParams.entries())
