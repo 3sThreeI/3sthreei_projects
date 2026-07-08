ad30555a7633f6b95096ac6947419a39b4de09b7
AbouCloud <Abouzari6839@gmail.com>
2026-07-08 16:08:59 +0000

    fix metadata: services gaming  page

--- app/[locale]/(marketing)/services/gaming/page.tsx
+++ app/[locale]/(marketing)/services/gaming/page.tsx
@@ -10,18 +10,43 @@ export async function generateMetadata(params: Promise<{ locale: string }>): Pro
         title: t("game.title"),
         description: t("game.description"),
         keywords: t.raw("game.keywords").join(", "),
+        alternates: {
+            canonical: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}/services/game`,
+            languages: {
+                fr: `${process.env.NEXT_PUBLIC_SITE_URL}/fr/services/game`,
+                en: `${process.env.NEXT_PUBLIC_SITE_URL}/en/services/game`,
+            },
+        },
         openGraph: {
             title: t("game.title"),
             description: t("game.description"),
-            url: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}/services/game`
+            url: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}/services/game`,
+            type: "website",
+            locale: locale === "fr" ? "fr_FR" : "en_US",
+            siteName: t('sitename'),
+            images: [
+                {
+                    url: t('ogImage'),
+                    width: 1200,
+                    height: 630,
+                    alt: t('ogImageAlt'),
+                },
+            ],
         },
-        alternates: {
-            canonical: `/${locale}/services/game`
+
+        twitter: {
+            card: "summary_large_image",
+            title: t("game.title"),
+            description: t("game.description"),
+            images: [t("ogImage")]
         },
         robots: {
             index: true,
             follow: true,
-        }
+        },
+        authors: [{ name: "3sthreei" }],
+        category: "3sthreei",
+        publisher: "3sthreei",
     }
 }
 export default async function Game({ params }: ParamsPropsServices) {
