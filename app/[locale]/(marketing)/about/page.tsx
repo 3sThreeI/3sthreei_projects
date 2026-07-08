5a96d733e45e9d1f7819d2b71564ac184b8e75a5
AbouCloud <Abouzari6839@gmail.com>
2026-07-08 16:01:33 +0000

    fix metadata: about page

--- app/[locale]/(marketing)/about/page.tsx
+++ app/[locale]/(marketing)/about/page.tsx
@@ -10,20 +10,46 @@ export interface ParamProps {
 export async function generateMetadata(params: Promise<{ locale: any }>): Promise<Metadata> {
     const { locale } = await params
     const t = await getTranslations({ locale, namespace: "about" })
-    const sitename = await getTranslations("sitename") as any
     return {
         title: t("title"),
         description: t('description'),
         keywords: t.raw('keywords').join(', '),
+        alternates: {
+            canonical: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}/about`,
+            languages: {
+                fr: `${process.env.NEXT_PUBLIC_SITE_URL}/fr/about`,
+                en: `${process.env.NEXT_PUBLIC_SITE_URL}/en/about`
+            }
+        },
         openGraph: {
             title: t("title"),
             description: t('description'),
-            siteName: sitename,
-            url: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}/about`
+            siteName: t('sitename'),
+            url: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}/about`,
+            type: "website",
+            locale: locale === "fr" ? 'fr_FR' : 'en_US',
+            images: [
+                {
+                    url: t("ogImage"),
+                    width: 1200,
+                    height: 630,
+                    alt: t("ogImageAlt"),
+                }
+            ]
         },
-        alternates: {
-            canonical: `/${locale}/about`,
+        twitter: {
+            card: "summary_large_image",
+            title: t("title"),
+            description: t('description'),
+            images: [t("ogImage")]
         },
+        robots: {
+            index: true,
+            follow: true,
+        },
+        authors: [{ name: "3sthreei" }],
+        category: "3sthreei",
+        publisher: "3sthreei",
     }
 }
 export default async function AboutUS({ params }: ParamProps) {
@@ -34,10 +60,10 @@ export default async function AboutUS({ params }: ParamProps) {
         <NextIntlClientProvider messages={message}>
             {/* -------------------about hero section */}
             <section>
-                <AbouHero message={message}/>
+                <AbouHero message={message} />
             </section>
             <section>
-                <AboutUs message={message}/>
+                <AboutUs message={message} />
             </section>
         </NextIntlClientProvider>
     )
