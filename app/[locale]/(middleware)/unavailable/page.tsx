c75a54f258a53307a93dc9211c11ca5ad0e2ff15
AbouCloud <Abouzari6839@gmail.com>
2026-07-08 16:11:01 +0000

    fix metadata: unavailable  page

--- app/[locale]/(middleware)/unavailable/page.tsx
+++ app/[locale]/(middleware)/unavailable/page.tsx
@@ -12,14 +12,42 @@ export async function generateMetadata(params: Promise<{ locale: string }>): Pro
         title: t('title'),
         description: t('description'),
         keywords: t.raw('keywords').join(', '),
+        alternates: {
+            canonical: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}/unavailable`,
+            languages: {
+                fr: `${process.env.NEXT_PUBLIC_SITE_URL}/fr/unavailable`,
+                en: `${process.env.NEXT_PUBLIC_SITE_URL}/en/unavailable`,
+            },
+        },
         openGraph: {
             title: t('title'),
             description: t('description'),
-            url: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}/unavailable`
+            url: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}/unavailable`,
+            type: "website",
+            locale: locale === "fr" ? "fr_FR" : "en_US",
+            siteName: t('sitename'),
+            images: [
+                {
+                    url: t("ogImage"),
+                    width:1200,
+                    height: 630,
+                    alt: t("ogImageAlt"),
+                },
+            ],
         },
-        alternates: {
-            canonical: `/${locale}/unavailable`
-        }
+        twitter: {
+            card: "summary_large_image",
+            title: t("title"),
+            description: t("description"),
+            images: [t("ogImage")]
+        },
+        robots: {
+            index: true,
+            follow: true,
+        },
+        authors: [{ name: "3sthreei" }],
+        category: "3sthreei",
+        publisher: "3sthreei",
     }
 }
 // Param props is importing from about page.tsx
@@ -33,7 +61,7 @@ export default async function Unavailable({ params }: ParamProps) {
                 <h1 className={style.TextLogo}>3<span className={style.span}>S</span>Three<span className={style.span}>I</span></h1>
             </div>
             <div className={style.content}>
-                <FaTools className={style.icon}/>
+                <FaTools className={style.icon} />
                 <p className={style.text}>{t.text1}</p>
                 <p>{t.text2}</p>
             </div>
