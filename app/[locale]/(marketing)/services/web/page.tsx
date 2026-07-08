a68e71351d09bd8b3259085403240f7d522c82df
AbouCloud <Abouzari6839@gmail.com>
2026-07-08 16:08:39 +0000

    fix metadata: services web  page

--- app/[locale]/(marketing)/services/web/page.tsx
+++ app/[locale]/(marketing)/services/web/page.tsx
@@ -9,47 +9,74 @@ import WebServiceMarketing from "@/app/[locale]/assets/servicesComp/web/WebServi
 export async function generateMetadata(params: Promise<{ locale: string }>): Promise<Metadata> {
     const { locale } = await params
     const t = await getTranslations({ locale, namespace: "services" })
-    const sitename = await getTranslations('sitenames') as any
     return {
         "title": t("web.title"),
         "description": t("web.description"),
         "keywords": t.raw("web.keywords"),
+        "alternates": {
+            "canonical": `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}/services/web`,
+            languages: {
+                fr: `${process.env.NEXT_PUBLIC_SITE_URL}/fr/services/web`,
+                en: `${process.env.NEXT_PUBLIC_SITE_URL}/en/services/web`,
+            },
+        },
         "openGraph": {
             "title": t("web.title"),
             "description": t("web.description"),
-            "siteName": sitename,
+            siteName: t('sitename'),
+            type: "website",
+            locale: locale === "fr" ? "fr_FR" : "en_US",
             url: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}/services/web`,
+            images: [
+                {
+                    url: t('ogImage'),
+                    width: 1200,
+                    height: 630,
+                    alt: t('ogImageAlt'),
+                },
+            ],
         },
-        "alternates": {
-            "canonical": `/${locale}/services/web`
-        }
+
+        twitter: {
+            card: "summary_large_image",
+            title: t("web.title"),
+            description: t("web.description"),
+            images: [t("ogImage")]
+        },
+        robots: {
+            index: true,
+            follow: true,
+        },
+        authors: [{ name: "3sthreei" }],
+        category: "3sthreei",
+        publisher: "3sthreei",
     }
 }
 export interface ParamsPropsServices {
-    params: {locale:string}
+    params: { locale: string }
 }
-export default async function Web({params}:ParamsPropsServices) {
-    const {locale} = await params
+export default async function Web({ params }: ParamsPropsServices) {
+    const { locale } = await params
     const messages = (await import(`@/messages/${locale}/services/web.json`)).default
     return (
         <>
-        <NextIntlClientProvider messages={messages}>
-            {/* h1 in servicesHero */}
-            <section>
-                <ServicesHero messages={messages}/>
-            </section>
-            {/* problem solving */}
-            {/* h3 h3 in problem solving  */}
-            <section>
-                <ProblemSolving messages={messages} type="web"/>
-            </section>
-            <section>
-                <WebServiceMarketing messages={messages}/>
-            </section>
-            <section>
-                <WorkingFlow messages={messages}/>
-            </section>
-        </NextIntlClientProvider>
+            <NextIntlClientProvider messages={messages}>
+                {/* h1 in servicesHero */}
+                <section>
+                    <ServicesHero messages={messages} />
+                </section>
+                {/* problem solving */}
+                {/* h3 h3 in problem solving  */}
+                <section>
+                    <ProblemSolving messages={messages} type="web" />
+                </section>
+                <section>
+                    <WebServiceMarketing messages={messages} />
+                </section>
+                <section>
+                    <WorkingFlow messages={messages} />
+                </section>
+            </NextIntlClientProvider>
         </>
     )
 }
