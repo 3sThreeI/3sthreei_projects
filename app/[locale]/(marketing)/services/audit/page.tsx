bd7d2952630f2c27eb85c56a6188b88e1a442aad
AbouCloud <Abouzari6839@gmail.com>
2026-07-08 16:08:16 +0000

    fix metadata: services audit  page

--- app/[locale]/(marketing)/services/audit/page.tsx
+++ app/[locale]/(marketing)/services/audit/page.tsx
@@ -7,28 +7,54 @@ import FormAudit from "@/components/customComponent/formContact/formAudit"
 export async function generateMetadata(params: Promise<{ locale: string }>): Promise<Metadata> {
     const { locale } = await params
     const t = await getTranslations({ locale, namespace: "contactForm" })
-    const sitename = await getTranslations('sitenames') as any
     return {
         title: t('title'),
         description: t('description'),
+        keywords: t.raw("keywords").join(", "),
+        alternates: {
+            canonical: `/${locale}/contact-form`,
+            languages: {
+                fr: `${process.env.NEXT_PUBLIC_SITE_URL}/fr/contact-form`,
+                en: `${process.env.NEXT_PUBLIC_SITE_URL}/en/contact-form`
+            }
+        },
         openGraph: {
             title: t('title'),
             description: t('description'),
-            "siteName": sitename,
-            url: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}/contact-form`
+            siteName: t('sitename'),
+            type: "website",
+            url: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}/contact-form`,
+            images: [
+                {
+                    url: t("ogImage"),
+                    width: 1200,
+                    height: 630,
+                    alt: t("ogImageAlt"),
+                },
+            ]
         },
-        alternates:{
-            canonical: `/${locale}/contact-form`
-        }
+        twitter: {
+            card: "summary_large_image",
+            title: t("title"),
+            description: t("description"),
+            images: [t("ogImage")]
+        },
+        robots: {
+            index: true,
+            follow: true,
+        },
+        authors: [{ name: "3sthreei" }],
+        category: "3sthreei",
+        publisher: "3sthreei",
     }
 }
 // ParamProps is importing from about page.tsx
-export default async function ProjectContactForm({params}:ParamProps) {
-    const {locale} = await params
+export default async function ProjectContactForm({ params }: ParamProps) {
+    const { locale } = await params
     const message = (await import(`@/messages/${locale}/forms.json`)).default
     return (
         <div className={style.container}>
-            <FormAudit message={message} searchParams=""/>
+            <FormAudit message={message} searchParams="" />
         </div>
     )
 }
