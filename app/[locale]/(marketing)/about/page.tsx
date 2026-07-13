10315bfc82cd79ee9757a9b9c09327a5cef20d83
AbouCloud <Abouzari6839@gmail.com>
2026-07-13 20:48:30 +0000

    JSONLD: about page

--- app/[locale]/(marketing)/about/page.tsx
+++ app/[locale]/(marketing)/about/page.tsx
@@ -8,7 +8,7 @@ export interface ParamProps {
     params: Promise<{ locale: string }>;
     searchParams: Promise<{ type?: string }>
 }
-export async function generateMetadata({params}:ParamProps): Promise<Metadata> {
+export async function generateMetadata({ params }: ParamProps): Promise<Metadata> {
     const { locale } = await params
     const t = await getTranslations({ locale, namespace: "about" })
     return {
@@ -56,16 +56,82 @@ export async function generateMetadata({params}:ParamProps): Promise<Metadata> {
 export default async function AboutUS({ params }: ParamProps) {
     const { locale } = await params
     const message = (await import(`@/messages/${locale}/about.json`)).default;
-    console.log("**********about page is has been rendere************")
+    const JsonT = (await import(`@/messages/${locale}/jsonLD/aboutJ.json`)).default
+    const BaseUrl = process.env.NEXT_PUBLIC_SITE_URL || "http://3sthreei.com"
+    // console.log("**********about page is has been rendere************", JsonT)
+    const aboutJSONLD = {
+        "@context": "https://schema.org",
+        "@type": "AboutPage",
+        "@id": `${BaseUrl}/${locale}/about#about-page`,
+        "url": `${BaseUrl}/${locale}/about`,
+        "name": JsonT.name,
+        "description": JsonT.description,
+        "isPartOf": {
+            "@id": `${BaseUrl}#website`,
+        },
+        "about": {
+            "@type": "Organization",
+            "@id": `${BaseUrl}#organization`,
+        },
+        "image": [
+            {
+                "@type": "ImageObject",
+                "url": `${BaseUrl}/icons/3sthreei_icon_Black.png`,
+                "caption": "3sthreei Company Logo"
+            },
+            {
+                "@type": "ImageObject",
+                "url": `${BaseUrl}/icons/3sthreei_icon_White.png`,
+                "caption": "3sthreei Company Logo"
+            }
+        ],
+        "primaryImageOfPage": {
+            "@type": "ImageObject",
+            "url": `${BaseUrl}/icons/3sthreei_icon_Black.png`,
+        },
+        "inLanguage": [
+            { "@type": "Language", "name": locale === "fr" ? "French" : "English", "alternateName": locale }
+        ],
+        "mainEntity": {
+            "@type": "Organization",
+            "@id": `${BaseUrl}#organization`
+        }
+    }
+    const aboutBreadcrumb = {
+        "@context": "https://schema.org",
+        "@type": "BreadcrumbList",
+        "itemListElement": [
+            {
+                "@type": "ListItem",
+                "position": 1,
+                "name": locale === "fr" ? "Accueil" : "Home",
+                "item": `${BaseUrl}/${locale}`
+            },
+            {
+                "@type": "ListItem",
+                "position": 2,
+                "name": locale === "fr" ? "À propos" : "About",
+                "item": `${BaseUrl}/${locale}/about`
+            },
+        ]
+    }
     return (
-        <NextIntlClientProvider messages={message}>
-            {/* -------------------about hero section */}
-            <section>
-                <AbouHero message={message} />
-            </section>
-            <section>
-                <AboutUs message={message} />
-            </section>
-        </NextIntlClientProvider>
+        <>
+            <script type="application/ld+json"
+                dangerouslySetInnerHTML={{ __html: JSON.stringify(aboutJSONLD).replace(/</g, '\\u003c'), }}
+            />
+            <script type="application/ld+json"
+                dangerouslySetInnerHTML={{ __html: JSON.stringify(aboutBreadcrumb).replace(/</g, '\\u003c'), }}
+            />
+            <NextIntlClientProvider messages={message}>
+                {/* -------------------about hero section */}
+                <section>
+                    <AbouHero message={message} />
+                </section>
+                <section>
+                    <AboutUs message={message} />
+                </section>
+            </NextIntlClientProvider>
+        </>
     )
 }
