4a61712a91077bcb2cde5324f522d9be7a0d12de
AbouCloud <Abouzari6839@gmail.com>
2026-07-13 20:48:49 +0000

    JSONLD: faq page

--- app/[locale]/(marketing)/faq/page.tsx
+++ app/[locale]/(marketing)/faq/page.tsx
@@ -3,9 +3,9 @@ import { NextIntlClientProvider } from "next-intl";
 import { getTranslations } from "next-intl/server";
 import Faq from "../../assets/faq/faq";
 type Props = {
-   params: Promise<{locale:string}>;
+    params: Promise<{ locale: string }>;
 }
-export async function generateMetadata({params}:Props): Promise<Metadata> {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
     const { locale } = await params
     const t = await getTranslations({ locale, namespace: "faq" })
     return {
@@ -53,13 +53,75 @@ export async function generateMetadata({params}:Props): Promise<Metadata> {
 export default async function OurFAQ({ params }: { params: any }) {
     const { locale } = await params
     const message = (await import(`@/messages/${locale}/faq.json`)).default
-    console.log("***************FAQ rendered **************")
+    const JsonT = (await import(`@/messages/${locale}/jsonLD/faqJ.json`)).default
+    const BaseUrl = process.env.NEXT_PUBLIC_SITE_URL || "http://3sthreei.com"
+    const FaqJSONLD = {
+        "@context": "https://schema.org",
+        "@type": "FAQPage",
+        "@id": `${BaseUrl}/${locale}/faq#faq-page`,
+        "url": `${BaseUrl}/${locale}/faq`,
+        "name": JsonT.name,
+        "description": JsonT.description,
+        "isPartOf": {
+            "@id": `${BaseUrl}#website`,
+        },
+        "about": {
+            "@type": "Organization",
+            "@id": `${BaseUrl}#organization`,
+        },
+        "image": [
+            {
+                "@type": "ImageObject",
+                "url": `${BaseUrl}/icons/3sthreei_icon_Black.png`,
+                "caption": "3sthreei Company Logo"
+            }
+        ],
+        "primaryImageOfPage": {
+            "@type": "ImageObject",
+            "url": `${BaseUrl}/icons/3sthreei_icon_Black.png`,
+        },
+        "inLanguage": [
+            { "@type": "Language", "name": locale === "fr" ? "French" : "English", "alternateName": locale }
+        ],
+        "mainEntity": [
+            {
+                "@type": "Question",
+                "name": "What services do you offer?",
+                "acceptedAnswer": {
+                    "@type": "Answer",
+                    "text": "We offer web design, development, e-commerce, gaming apps, and more."
+                }
+            }
+        ]
+    }
+    const FaqBreadcrumb = {
+        "@context": "https://schema.org",
+        "@type": "BreadcrumbList",
+        "itemListElement": [
+            { "@type": "ListItem", "position": 1, "name": "Home", "item": `${BaseUrl}/${locale}` },
+            { "@type": "ListItem", "position": 2, "name": "Faq", "item": `${BaseUrl}/${locale}/faq` },
+        ]
+    }
+    // console.log("***************FAQ rendered **************")
     return (
-        <NextIntlClientProvider messages={message}>
-            {/* faq questions */}
-            <section>
-                <Faq messages={message} />
-            </section>
-        </NextIntlClientProvider>
+        <>
+            <script
+                type="application/ld+json"
+                dangerouslySetInnerHTML={{ __html: JSON.stringify(FaqJSONLD).replace(/</g, '\\u003c'), }}
+            />
+            <script
+                type="application/ld+json"
+                dangerouslySetInnerHTML={{ __html: JSON.stringify(FaqBreadcrumb).replace(/</g, '\\u003c'), }}
+            />
+            <NextIntlClientProvider messages={message}>
+                <main>
+                    {/* faq questions */}
+                    <section>
+                        <Faq messages={message} />
+                    </section>
+                </main>
+            </NextIntlClientProvider>
+        </>
+
     )
 }
