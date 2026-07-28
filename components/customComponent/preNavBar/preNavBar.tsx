fccf41f6ebde3920839b544bb9cc1b8f1b155dc2
AbouCloud <Abouzari6839@gmail.com>
2026-07-28 14:50:12 +0000

    navbar updating

---
+++ components/customComponent/preNavBar/preNavBar.tsx
@@ -0,0 +1,17 @@
+
+
+export default function PreNavBar(){
+    return (
+        <>
+        </>
+    )
+}
+
+{/*
+    {
+  "isBeta": true,
+  "discount": 30,
+  "endDate": "2027-01-31T23:59:59Z",
+  "message": "Beta Launch"
+}
+  */}
