e3ed27a8a4a93ec712867e4856acb20b5b022541
AbouCloud <Abouzari6839@gmail.com>
2026-07-30 21:57:09 +0000

    Performance: home page

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
 import createNextIntlPlugin from "next-intl/plugin";
+import bundleAnalyzer from "@next/bundle-analyzer";
 import type { NextConfig } from "next";
 
 const withNextIntl = createNextIntlPlugin();
 
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
 const nextConfig: NextConfig = {
- 
-  // Add any other Next.js config options here
+  // Your Next.js config options
 };
 
-export default withNextIntl(nextConfig);
+export default withBundleAnalyzer(withNextIntl(nextConfig));
