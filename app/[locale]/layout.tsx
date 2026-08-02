971b4a97c9cfb270aa45e5e845a42dce391d4583
AbouCloud <Abouzari6839@gmail.com>
2026-08-02 03:46:17 +0000

    beta fixing

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -89,7 +89,7 @@ const GetBetaVersionInfo =  async ()=>{
   const resp = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/beta/test-3si`, {
     method: "GET",
     cache: "no-store"
-  },)
+  })
   const data = await resp.json()
   if(!resp.ok){
     return false
